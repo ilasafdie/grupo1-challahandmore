37e9791bd017c7f03759c70255813aba520ae99f
danacharnis <109525928+danacharnis@users.noreply.github.com>
2022-08-31 11:37:18 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require ("express");
+const path = require ("path");
+
+const app = express();
+app.use(express.static("public"))
+
+app.listen(3000,()=> {
+    console.log("servidor escuchando puerto 3000")
+});
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname,  "./views/home.html"))
+});
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+})
+
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/carrito.html'));
+})
+
+app.get('/detailsproducts', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/detailsproducts.html'));
+})
+
