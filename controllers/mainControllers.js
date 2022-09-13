657ca9fb71ab27f37ec3ee4405da558cd57a2042
danacharnis <109525928+danacharnis@users.noreply.github.com>
2022-09-13 10:52:26 +0300

    Update mainControllers.js

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,18 +1,24 @@
 const controller = {
     getIndex: function(req, res){
-        res.send ("estas en Index");
+        res.send ("estas en Index") },
     
     getHome: function(req, res){
-        res.send ("estas en HOME");
+        res.send ("estas en HOME") },
 
     getLogin: function(req, res){
-        res.send ("estas en login");
+        res.send ("estas en login") },
 
-    getRegister: function(req, res){
-        res.send ("estas en register");
+    getRegister: function (req, res){
+        res.send ("estas en register") },
+
+    getCarrito: function(req, res){
+            res.send ("estas en carrito") },
+
+    getDetailProduct: function(req, res){
+                res.send ("estas en DetailProduct") }
 
     }
     
-}
+
 
 module.express = controller;
