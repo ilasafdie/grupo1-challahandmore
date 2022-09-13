7083303e1a00864419a476483d91dce8e4a435a2
danacharnis <109525928+danacharnis@users.noreply.github.com>
2022-09-13 10:19:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
 const express = require ("express");
-const path = require ("path");
+const mainRoutes= require("./routes/mainRoutes");
 
 const app = express();
+
+app.use (mainRoutes)
+
 app.use(express.static("public"))
 
 app.listen(3000,()=> {
     console.log("servidor escuchando puerto 3000")
 });
 
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname,  "./views/home.html"))
-});
-app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html'));
-})
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
-})
-
-app.get('/carrito', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/carrito.html'));
-})
-
-app.get('/detailsproducts', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/detailsproducts.html'));
-})
 
