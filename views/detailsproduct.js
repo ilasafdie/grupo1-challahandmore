cef6552584bd85b9dfade3eafc80a6a3430babb3
danacharnis <109525928+danacharnis@users.noreply.github.com>
2022-09-13 10:19:57 +0300

    Create detailsproduct.js

---
+++ views/detailsproduct.js
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="/css/styles.css">
+    <title>
+        products details
+    </title>
+</head>
+
+<body>
+    <header>  
+    
+        <div class="menu-base">
+        
+             <div class="botones">
+                <button type="submit" class="menu-arriba1"><a href="./home.html"><img width="70px" height="70px" src="/images/challa.jpeg" class="logo" alt="logo"> </a></button>
+                <button type="submit" class="menu-arriba"><a href="/"> MENU </a></button>     
+                <button type="submit" class="menu-arriba"><a href="/"> KOSHER WHAT? </a></button>  
+                <button type="submit" class="menu-arriba"><a href="/"> ABOUT US </a></button>  
+                <button type="submit" class="menu-arriba"><a href="/"> LOGIN </a></button> 
+                <button type="submit" class="menu-arriba"><a href="/"> PROFILE </a></button>
+                <button type="submit" class="menu-arriba1"><a href="/"><i class='fas fa-shopping-basket fa-2x'></i></button>
+    
+            </div>
+            </div>
+    
+        </div>
+    </div>
+    </header>
+    <main>
+        <h3>Nuestros productos</h3>
+        <section>
+          <article class="hola"> 
+            <div class="productos">
+                 <img width="150" height="150" src="/images/deserts/bagel.jpeg" alt="bagel">
+                 <p>Carrot Cake</p>
+            <p class="precios">$1000</p>
+            <div class="cant">
+                <label class="screen-reader-text" for="quantity"></label>
+                <input type="number" id="quantity" class="input" step="1" min="0" max name="cart" value="1" title="Cantidad" size="4" placeholder inputmode="numeric" autocomplete="off">
+                <div class="envio">
+                    <button type="submit" class="envio"><a href="/">Agregar al Carrito</a></button>
+                </div>
+            </div>
+            </div>
+        </article> 
+        <article class="hola"> 
+            <div class="productos">
+                 <img width="150" height="150" src="/images/deserts/budin.jpeg" alt="budin">
+                 <p>Budin con Chispitas</p>
+            <p class="precios">$1000</p>
+            <div class="cant">
+                <label class="screen-reader-text" for="quantity"></label>
+                <input type="number" id="quantity" class="input" step="1" min="0" max name="cart" value="1" title="Cantidad" size="4" placeholder inputmode="numeric" autocomplete="off">
+                <div class="envio">
+                    <button type="submit" class="envio"><a href="/">Agregar al Carrito</a></button>
+                </div>
+            </div>
+            </div>
+        </article> 
+        <article class="hola"> 
+            <div class="productos">
+                 <img width="150" height="150" src="/images/deserts/selvanegra-1.jpeg" alt="selva">
+                 <p>Selva Negra</p>
+            <p class="precios">$1000</p>
+            <div class="cant">
+                <label class="screen-reader-text" for="quantity"></label>
+                <input type="number" id="quantity" class="input" step="1" min="0" max name="cart" value="1" title="Cantidad" size="4" placeholder inputmode="numeric" autocomplete="off">
+                <div class="envio">
+                    <button type="submit" class="envio"><a href="/">Agregar al Carrito</a></button>
+                </div>
+            </div>
+            </div>
+        </article> 
+        <article class="hola"> 
+            <div class="productos">
+                 <img width="150" height="150" src="/images/cakes/tortacapascolores.jpeg" alt="crutons">
+                 <p>Torta de Manzana</p>
+            <p class="precios">$1000</p>
+            <div class="cant">
+                <label class="screen-reader-text" for="quantity"></label>
+                <input type="number" id="quantity" class="input" step="1" min="0" max name="cart" value="1" title="Cantidad" size="4" placeholder inputmode="numeric" autocomplete="off">
+                <div class="envio">
+                    <button type="submit" class="envio"><a href="/">Agregar al Carrito</a></button>
+                </div>
+            </div>
+            </div>
+        </article> 
+        <article class="hola"> 
+            <div class="productos">
+                 <img width="150" height="150" src="/images/deserts/kujelarandanos.jpeg" alt="budin">
+                 <p>Kujel de Arandanos</p>
+            <p class="precios">$1000</p>
+            <div class="cant">
+                <label class="screen-reader-text" for="quantity"></label>
+                <input type="number" id="quantity" class="input" step="1" min="0" max name="cart" value="1" title="Cantidad" size="4" placeholder inputmode="numeric" autocomplete="off">
+                <div class="envio">
+                    <button type="submit" class="envio"><a href="/">Agregar al Carrito</a></button>
+                </div>
+            </div>
+            </div>
+        </article> 
+        <article class="hola"> 
+            <div class="productos">
+                 <img width="150" height="150" src="/images/deserts/stroisel.jpeg" alt="stroisel">
+                 <p>Stroilsel</p>
+            <p class="precios">$1000</p>
+            <div class="cant">
+                <label class="screen-reader-text" for="quantity"></label>
+                <input type="number" id="quantity" class="input" step="1" min="0" max name="cart" value="1" title="Cantidad" size="4" placeholder inputmode="numeric" autocomplete="off">
+                <div class="envio">
+                    <button type="submit" class="envio"><a href="/">Agregar al Carrito</a></button>
+                </div>
+            </div>
+            </div>
+        </article> 
+        <article class="hola"> 
+            <div class="productos">
+                 <img width="150" height="150" src="/images/cakes/tortatcapas.jpeg" alt="capas">
+                 <p>Crocante</p>
+            <p class="precios">$1000</p>
+            <div class="cant">
+                <label class="screen-reader-text" for="quantity"></label>
+                <input type="number" id="quantity" class="input" step="1" min="0" max name="cart" value="1" title="Cantidad" size="4" placeholder inputmode="numeric" autocomplete="off">
+                <div class="envio">
+                    <button type="submit" class="envio"><a href="/">Agregar al Carrito</a></button>
+                </div>
+            </div>
+            </div>
+        </article> 
+        <article class="hola"> 
+            <div class="productos">
+                 <img width="150" height="150" src="/images/cakes/tortatfrutilla.jpeg" alt="frutilla">
+                 <p>Tarta de Frutilla</p>
+            <p class="precios">$1000</p>
+            <div class="cant">
+                <label class="screen-reader-text" for="quantity"></label>
+                <input type="number" id="quantity" class="input" step="1" min="0" max name="cart" value="1" title="Cantidad" size="4" placeholder inputmode="numeric" autocomplete="off">
+                <div class="envio">
+                    <button type="submit" class="envio"><a href="/">Agregar al Carrito</a></button>
+                </div>
+            </div>
+            </div>
+        </article> 
+    </section>
+        <section class="masproducts">
+        <article class="mas">
+            <div class="productos">
+                <img width="150" height="150" src="/images/shabat & jaguim/2jalot.jpeg" alt="jalot">
+                <p>Jalot</p>
+           <p class="precios">$1000</p>
+           <div class="cant">
+               <label class="screen-reader-text" for="quantity"></label>
+               <input type="number" id="quantity" class="input" step="1" min="0" max name="cart" value="1" title="Cantidad" size="4" placeholder inputmode="numeric" autocomplete="off">
+               <div class="envio">
+                   <button type="submit" class="envio"><a href="/">Agregar al Carrito</a></button>
+               </div>
+           </div>
+           </div>
+        </article>
+        <article class="mas">
+            <div class="productos">
+                <img width="150" height="150" src="/images/shabat & jaguim/boios.jpeg" alt="boios">
+                <p>boios</p>
+           <p class="precios">$1000</p>
+           <div class="cant">
+               <label class="screen-reader-text" for="quantity"></label>
+               <input type="number" id="quantity" class="input" step="1" min="0" max name="cart" value="1" title="Cantidad" size="4" placeholder inputmode="numeric" autocomplete="off">
+               <div class="envio">
+                   <button type="submit" class="envio"><a href="/">Agregar al Carrito</a></button>
+               </div>
+           </div>
+           </div>
+        </article>
+        <article class="mas">
+            <div class="productos">
+                <img width="150" height="150" src="/images/shabat & jaguim/lajmashin.jpeg" alt="frutilla">
+                <p>lajmashin</p>
+           <p class="precios">$1000</p>
+           <div class="cant">
+               <label class="screen-reader-text" for="quantity"></label>
+               <input type="number" id="quantity" class="input" step="1" min="0" max name="cart" value="1" title="Cantidad" size="4" placeholder inputmode="numeric" autocomplete="off">
+               <div class="envio">
+                   <button type="submit" class="envio"><a href="/">Agregar al Carrito</a></button>
+               </div>
+           </div>
+           </div>
+        </article>
+        <article class="mas">
+            <div class="productos">
+                <img width="150" height="150" src="/images/shabat & jaguim/shakshuka.jpg" alt="frutilla">
+                <p>Shakshuka</p>
+           <p class="precios">$1000</p>
+           <div class="cant">
+               <label class="screen-reader-text" for="quantity"></label>
+               <input type="number" id="quantity" class="input" step="1" min="0" max name="cart" value="1" title="Cantidad" size="4" placeholder inputmode="numeric" autocomplete="off">
+               <div class="envio">
+                   <button type="submit" class="envio"><a href="/">Agregar al Carrito</a></button>
+               </div>
+           </div>
+           </div>
+        </article>
+        <article class="mas">
+            <div class="productos">
+                <img width="150" height="150" src="/images/tarts/tartabroc.jpeg" alt="tarta">
+                <p>Tarta de Brocoli</p>
+           <p class="precios">$1000</p>
+           <div class="cant">
+               <label class="screen-reader-text" for="quantity"></label>
+               <input type="number" id="quantity" class="input" step="1" min="0" max name="cart" value="1" title="Cantidad" size="4" placeholder inputmode="numeric" autocomplete="off">
+               <div class="envio">
+                   <button type="submit" class="envio"><a href="/">Agregar al Carrito</a></button>
+               </div>
+           </div>
+           </div>
+        </article>
+        <article class="mas">
+            <div class="productos">
+                <img width="150" height="150" src="/images/tarts/tartaqueso.jpeg" alt="tarta">
+                <p>Tarta de Queso</p>
+           <p class="precios">$1000</p>
+           <div class="cant">
+               <label class="screen-reader-text" for="quantity"></label>
+               <input type="number" id="quantity" class="input" step="1" min="0" max name="cart" value="1" title="Cantidad" size="4" placeholder inputmode="numeric" autocomplete="off">
+               <div class="envio">
+                   <button type="submit" class="envio"><a href="/">Agregar al Carrito</a></button>
+               </div>
+           </div>
+           </div>
+        </article>
+        <article class="mas">
+            <div class="productos">
+                <img width="150" height="150" src="/images/tarts/tartazapallito.jpeg" alt="tarta">
+                <p>Tarta de Zapallito</p>
+           <p class="precios">$1000</p>
+           <div class="cant">
+               <label class="screen-reader-text" for="quantity"></label>
+               <input type="number" id="quantity" class="input" step="1" min="0" max name="cart" value="1" title="Cantidad" size="4" placeholder inputmode="numeric" autocomplete="off">
+               <div class="envio">
+                   <button type="submit" class="envio"><a href="/">Agregar al Carrito</a></button>
+               </div>
+           </div>
+           </div>
+        </article>
+        <article class="mas">
+            <div class="productos">
+                <img width="150" height="150" src="/images/dips/dip1.jpeg" alt="dip">
+                <p>Dips</p>
+           <p class="precios">$1000</p>
+           <div class="cant">
+               <label class="screen-reader-text" for="quantity"></label>
+               <input type="number" id="quantity" class="input" step="1" min="0" max name="cart" value="1" title="Cantidad" size="4" placeholder inputmode="numeric" autocomplete="off">
+               <div class="envio">
+                   <button type="submit" class="envio"><a href="/">Agregar al Carrito</a></button>
+               </div>
+           </div>
+           </div>
+        </article>
+    </section>
+
+    </main>
+    <footer>
+        <div class="footer-main">
+            <div class="footer-top-box">
+                <h3>Social Media</h3>
+                <ul>
+                    <p>Follow in: </p>
+                    <a href="#"><i class="fab fa-facebook" aria-hidden="true"></i> Facebook</a>
+                    <a href="#"><i class="fab fa-twitter" aria-hidden="true"></i> Twitter</a>
+                    <a href="#"><i class="fab fa-instagram" aria-hidden="true"></i> Instagram</a>
+                </ul>
+            </div>
+        </div>
+
+        <div class="footer-copyright">
+            <p>Made with <i class="fas fa-heart"></i> by DIGITAL HOUSE - Worldwide 2022 - All Rights Reserved. </p>
+        </div>
+    </footer>
+</body>
+
+</html>
