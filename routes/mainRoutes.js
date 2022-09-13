33df4adc33586955e4c891ea5dbd7a67fcea3651
danacharnis <109525928+danacharnis@users.noreply.github.com>
2022-09-13 10:52:30 +0300

    Update mainRoutes.js

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -4,5 +4,12 @@ const mainControllers = require ("../controllers/mainControllers");
 const router = express.Router();
 
 router.get ("/",mainControllers.getIndex);
+router.get ("/",mainControllers.getHome);
+router.get ("/",mainControllers.getCarrito);
+router.get ("/",mainControllers.getLogin);
+router.get ("/",mainControllers.getRegister);
+router.get ("/",mainControllers.getDetailProduct);
+router.get ("/",mainControllers.getUsers);
+
 
 module.exports= router;
