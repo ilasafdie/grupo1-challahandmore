a93fd4ddb9416ba805673c72d20947154779a154
Malka Jacobo <machasbat@yahoo.com.ar>
2022-10-23 14:57:55 -0300

    resolviendo errores de css por links rotos

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -10,7 +10,7 @@ router.get ("/register",mainControllers.getRegister);
 router.get ("/error404"), mainControllers.getError; /*pagina de error*/
 router.get ("/shopping",mainControllers.getShopping);
 
-router.post ("/productCreate",mainControllers.postCreate); /*Crear productos y cargarlos al listado final*/
+router.post ("/productCreatePost",mainControllers.postCreate); /*Crear productos y cargarlos al listado final*/
 router.get ("/productCreate",mainControllers.getCreate)
 router.get ("/productList/:search?",mainControllers.getProductList) /*listado de productos*/
 router.get ("/productEdit/:idProduct",mainControllers.getEdit); /*formulario de edicion de un producto*/ 
