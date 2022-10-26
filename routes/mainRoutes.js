192fef7d997974329ced3d29a09c64f81c41f1e6
danacharnis <109525928+danacharnis@users.noreply.github.com>
2022-10-26 14:32:12 +0300

    Update mainRoutes.js

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -9,5 +9,15 @@ router.get ("/register", mainControllers.getRegister);
 router.get ("/error404"), mainControllers.getError; /*pagina de error*/
 router.get ("/shopping", mainControllers.getShopping);
 
+router.get('/', function(req, res){
+    const usuario = req.cookies.nombre;
+    res.send ('Bienvenido usuario: ' usuario);
+})
+
+router.get('/register/:name', function(req, res){
+    let nombre = 'nombre' , nombre);
+    res.send ('cooookies')
+})
+
 
 module.exports= router;
