0e49911cb1558f17302952774a130518589fa20e
danacharnis <109525928+danacharnis@users.noreply.github.com>
2022-10-27 09:16:52 +0300

    Update users.js

--- models/users.js
+++ models/users.js
@@ -2,11 +2,15 @@ const fs = require ('fs');
 const users = {
     fileName: "./data/usersList.json",
     getData: function(){
-        return fs.readFileSync(this.fileName, 'utf-8');
+        return JSON.parse(fs.readFileSync(this.fileName, 'utf-8'));
+    },
+
+    findAll: function (){
+        return this.getData();
     },
     create: function(userData){
 
     }
 }
 
-console.log( users.getData());
+console.log( users.findAll());
