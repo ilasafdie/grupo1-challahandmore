93c8c554e8c5e52bfcdfc401beb7bd1c2c4a6fc7
danacharnis <109525928+danacharnis@users.noreply.github.com>
2022-10-27 10:01:05 +0300

    registro de usuarios

--- models/users.js
+++ models/users.js
@@ -5,12 +5,42 @@ const users = {
         return JSON.parse(fs.readFileSync(this.fileName, 'utf-8'));
     },
 
+    generateId: function (params){
+        let allUsers = this.findAll();
+        let lastUser = allUsers.pop();
+        if (lastUser){ 
+        return lastUser.id + 1;
+    }
+    return 1;
+     },
+
     findAll: function (){
         return this.getData();
     },
+
+    findByPK: function(id){
+        let allUsers = this.findAll();
+        let usersFound = allUsers.find ( oneUser => oneUser.id === id);
+        return usersFound;
+    },
+
+    findByField: function(field, text){
+        let allUsers = this.findAll();
+        let usersFound = allUsers.find ( oneUser => oneUser[field] === text);
+        return usersFound;
+    },
+
     create: function(userData){
+        let allUsers = this.findAll();
+        let newUser = {
+            id: this.generateId(),
+            ...userData
+        }
+        allUsers.push(newUser);
+        fs.writeFileSync(this.fileName, JSON.stringify(allUsers, null, ' '));
+        return true;
 
     }
 }
 
-console.log( users.findAll());
+console.log(users.create({Username:'Dana', Email: ' charnisdana@gmail.com' }));
