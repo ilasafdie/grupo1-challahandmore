547bd8e398cee2e1fd86d2718867d028cd37ed5e
Malka Jacobo <machasbat@yahoo.com.ar>
2022-10-27 23:25:24 -0300

    eliminado multer del users porque no lo usaremos

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,23 +1,10 @@
 const express = require( "express");
 const userControllers = require ("../controllers/userController");
-const multer = require('multer');
 const path = require('path');
 const router = express.Router();
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb){
-        cb(null, "./public/images/avatares");
-    },
-    filename: function(req, file, cb){
-        cb(null,  file.originalname);
-    }
-});
-
-const uploadFile = multer ({storage});
-
-
 router.get ("/register", userControllers.register); /* Formulario de Registro*/
-router.post ("/register", uploadFile.single ("photo"), userControllers.processRegister); /*Crear usuarios y cargarlos al json*/
+router.post ("/register", userControllers.processRegister); /*Crear usuarios y cargarlos al json*/
 
 router.post ("/login",userControllers.processLogin);/*Login*/
 router.get("/password:id",userControllers.processEditPassword);  /*Formulario de cambio de contraseña*/
@@ -25,7 +12,7 @@ router.post("/password",userControllers.editPassword); /*modifica la contraseña
 
 router.get("/profile/:id",userControllers.profileView); /*Muestra el perfil del usuario*/
 router.get ("/usersEdit/:idUser", userControllers.userEdit); /*la vista del formulario de edicion de un usuarios*/ 
-router.post ("/usersEdit", uploadFile.single ("photo"), userControllers.processUserEdit); /*Hace los cambios en el formulario de edicion de un usuarios*/ 
+router.post ("/usersEdit", userControllers.processUserEdit); /*Hace los cambios en el formulario de edicion de un usuarios*/ 
 router.post ("/usersDelete/:id",userControllers.userDelete); /*Eliminar usuarios*/
 
 router.post("/:id",userControllers.logout); /*desde algun boton aplica la funcionalidad de desloguearse*/
